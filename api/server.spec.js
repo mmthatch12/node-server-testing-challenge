f89b3b2c3ba379d665224ee6f4129dd25cdceb5a
mmthatch12 <50630435+mmthatch12@users.noreply.github.com>
2019-09-19 14:18:05 -0600

    created first test

--- api/server.spec.js
+++ api/server.spec.js
@@ -2,3 +2,14 @@ const request = require('supertest')
 
 const server = require('./server')
 
+describe('server.js',  () => {
+    describe('get /', () => {
+        it('returns 200', () => {
+            return request(server)
+            .get('/')
+            .then(res => {
+                expect(res.status).toBe(200)
+            })
+        })
+    })
+})
